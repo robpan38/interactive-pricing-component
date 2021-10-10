82c046a007b050429c9a19f423273f5995cef763
Robert Pandele <r.pandele38@gmail.com>
2021-10-10 15:38:34 +0300

    finished app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
 import './App.css';
+import { useState } from 'react';
+import useMediaQuery from './useMediaQuery';
 import Title from './components/Title';
 import Slider from './components/Slider';
 
 function App() {
+  const AppData = [
+    {
+      views: '10K',
+      monthly: '$8.00',
+      yearly: '$6.00'
+    },
+    {
+      views: '50K',
+      monthly: '$12.00',
+      yearly: '$9.00'
+    },
+    {
+      views: '100K',
+      monthly: '$16.00',
+      yearly: '$12.00'
+    },
+    {
+      views: '500K',
+      monthly: '$24.00',
+      yearly: '$18.00'
+    },
+    {
+      views: '1M',
+      monthly: '$36.00',
+      yearly: '$27.00'
+    }
+  ]
+
+  const [toggle, setToggle] = useState(false);
+  
+  const isScreenSmall = useMediaQuery("(max-width: 1250px)");
+
+  const handleToggleClick = () => {
+    setToggle(!toggle);
+  }
+
   return (
     <div className="root2">
       <Title/>
-      <Slider/>
+      <Slider appData={AppData} handleToggle={handleToggleClick} toggle={toggle} isScreenSmall={isScreenSmall}></Slider>
     </div>
   );
 }
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,18 +1,79 @@
 import './Slider.css';
+import { useState } from 'react';
+import check from '../images/icon-check.svg';
 
-const Slider = () => {
+const Slider = (props) => {
+    const [sliderValue, setSliderValue] = useState(50);
+    
     const onSliderChange = () => {
-        console.log(document.querySelector(".slider").value);
+        setSliderValue(Number(document.querySelector(".slider").value));
     }
     
+    const handleToggleClick = () => {
+        const toggle = document.querySelector(".toggleBillWrapper");
+        toggle.classList.toggle("toggleBillWrapperActivated");
+        props.handleToggle();
+    }
+
+    let appDataIndex;
+    if(sliderValue <= 20) {
+        appDataIndex = 0;
+    }
+    else if(sliderValue > 20 && sliderValue <= 40) {
+        appDataIndex = 1;
+    }
+    else if(sliderValue > 40 && sliderValue <= 60) {
+        appDataIndex = 2;
+    }
+    else if(sliderValue > 60 && sliderValue <= 80) {
+        appDataIndex = 3;
+    }
+    else {
+        appDataIndex = 4;
+    }
+
     return (
         <div className="sliderWrapper">
             <div className="sliderHeader">
-                <p className="pageviews">100K PAGEVIEWS</p>
-                <p className="price"></p>
+                <p className="pageViews">{props.appData[appDataIndex].views} PAGEVIEWS</p>
+                <p className="price">
+                    <span style={{fontSize: "max(2rem, 2vw)", fontWeight: "800", color: "hsl(227, 35%, 25%)"}}>
+                        {props.toggle === false ? props.appData[appDataIndex].monthly : props.appData[appDataIndex].yearly}
+                    </span>
+                     / month     
+                </p>
             </div>
             <input onChange={onSliderChange} className="slider" type="range" min="1" max="100" step="1"></input>
-            
+            <div className="billingWrapper">
+                <p className="bill">Monthly Billing</p>
+                <div className="toggleBillWrapper" onClick={handleToggleClick}>
+                    <div className="toggleBill"></div>
+                </div>
+                <p className="bill">Yearly Billing</p>
+                <div className="discount">
+                    <p className="discountText">
+                        {props.isScreenSmall === true ? "25%" : "25% discount"}
+                    </p>
+                </div>
+            </div>
+            <hr></hr>
+            <div className="sliderFooter">
+                <div className="checksWrapper">
+                    <div className="checkWrapper">
+                        <img src={check} alt="check"></img>
+                        <p>Unlimited websites</p>
+                    </div>
+                    <div className="checkWrapper">
+                        <img src={check} alt="check"></img>
+                        <p>100% data ownership</p>
+                    </div>
+                    <div className="checkWrapper">
+                        <img src={check} alt="check"></img>
+                        <p>Email reports</p>
+                    </div>
+                </div>
+                <button>Start my trial</button>
+            </div>
         </div>
     );
 }
